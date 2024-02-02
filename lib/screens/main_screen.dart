bb60814b81ba253971058ae2da5976c5a750603b
Mahdinkh7 <mahdinkh7@gmail.com>
2024-02-02 23:23:32 +0330

    add loading

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,31 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/current_balance.dart';
 import '../widgets/my_goals.dart';
 import '../widgets/transactions.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  bool loading = true;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1)).then((value) {
+      setState(() {
+        loading = false;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            CurrentBalance(
-              balance: '245,000.65',
-              growth: '1.76',
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: loading == true
+          ? const Center(
+              child: CupertinoActivityIndicator(
+              radius: 15,
+            ))
+          : const SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  CurrentBalance(
+                    balance: '245,000.65',
+                    growth: '1.76',
+                  ),
+                  SizedBox(height: 30),
+                  Transactions(),
+                  SizedBox(height: 20),
+                  MyGoals(),
+                ],
+              ),
             ),
-            SizedBox(height: 30),
-            Transactions(),
-            SizedBox(height: 20),
-            MyGoals(),
-          ],
-        ),
-      ),
     );
   }
 }
